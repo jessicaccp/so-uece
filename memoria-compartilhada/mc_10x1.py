51e3f34588347585a2fc579eee1f620880dd96e4
Jessica Cacau <jessicacacau@gmail.com>
2018-09-26 12:07:50 -0300

    Implementa leitura correta de todos os dados escritos

--- memoria-compartilhada/mc_10x1.py
+++ memoria-compartilhada/mc_10x1.py
@@ -2,16 +2,23 @@ from multiprocessing import Process, Lock
 from multiprocessing.sharedctypes import Value, Array
 import os
 
-def leitura(num, pid, lock):
+def leitura(msg, lock):
     lock.acquire()
-    print("Processo %s recebeu o valor %s do processo %s"
-            % (os.getpid(), num.value, pid.value))
+    for x in range(0, 20, 2):
+        print("Processo %s recebeu o valor %s do processo %s"
+                % (os.getpid(), msg[x], msg[x + 1]))
     lock.release()
 
-def escrita(num, pid, lock):
+def escrita(num, pid, msg, lock):
     lock.acquire()
+
     num.value += 1
     pid.value = os.getpid()
+
+    x = (num.value - 1) * 2
+    msg[x] = num.value
+    msg[x + 1] = pid.value
+
     lock.release()
 
 if __name__ == '__main__':
@@ -19,18 +26,20 @@ if __name__ == '__main__':
 
     num = Value('i', 0)
     pid = Value('i', 0)
+    msg = Array('i', range(20))
 
     origem = []
     for _ in range(10):
-        p = Process(target=escrita, args=(num, pid, lock))
+        p = Process(target=escrita, args=(num, pid, msg, lock))
         origem.append(p)
 
-    destino = Process(target=leitura, args=(num, pid, lock))
+    destino = Process(target=leitura, args=(msg, lock))
 
     for p in origem:
         p.start()
-    destino.start()
 
     for p in origem:
         p.join()
+
+    destino.start()
     destino.join()
