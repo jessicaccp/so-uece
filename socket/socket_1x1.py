301d9ad8f886abf0ad506bd0b57063a10cc79aef
Jessica Cacau <jessicacacau@gmail.com>
2018-09-27 11:41:53 -0300

    Configura mensagem a ser enviada e buffer

--- socket/socket_1x1.py
+++ socket/socket_1x1.py
@@ -1,32 +1,33 @@
 import socket, os
 from multiprocessing import Process
 
-def server():
-    host = 'localhost'
-    porta = 11111
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, porta))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
-
 def client():
-    host = 'localhost'
-    porta = 11111
+    ip = '127.0.0.1'
+    porta = 54321
+    mensagem = "\"Saudações do processo %s!\"" % os.getpid()
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, porta))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, porta))
+    s.send(mensagem.encode())
+    s.close()
 
-    print('Received', repr(data))
+def server():
+    ip = '127.0.0.1'
+    porta = 54321
+    buffer = 1024
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip, porta))
+    s.listen(1)
+
+    conexao, endereco = s.accept()
+    with conexao:
+        while True:
+            mensagem = conexao.recv(buffer)
+            if not mensagem:
+                break
+            print("Processo %s recebeu: %s" % (os.getpid(), mensagem.decode()))
+        conexao.close()
 
 if __name__ == '__main__':
     origem = Process(target=client)
